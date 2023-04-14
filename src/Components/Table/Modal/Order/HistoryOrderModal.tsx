5d8f4b05830993a7bb73e4e51e0040fc07f820e7
Oumardev <oumsdev@gmail.com>
2023-04-14 00:25:08 +0000

    ✨ feat(HistoryOrderModal.tsx, ReportOrderModal.tsx): add modal backdrop and form body with submit button and input fields.

--- src/Components/Table/Modal/Order/HistoryOrderModal.tsx
+++ src/Components/Table/Modal/Order/HistoryOrderModal.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ModalWrapper from '../ModalWrapper'
 
-export default function HistoryOrderModal() {
+interface Props {
+  showModal: boolean,
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+export default function HistoryOrderModal({ showModal, setShowModal }: Props) {
+  
+  useEffect(() => {
+    var body = document.querySelector<HTMLBodyElement>('body');
+
+    var modalBackdrop = document.createElement('div');
+    modalBackdrop.className = 'modal-backdrop fade show';
+
+    if (body) {
+      body.classList.add('modal-open');
+      body.style.overflow = 'hidden';
+      body.style.paddingRight = '17px';
+
+      body.appendChild(modalBackdrop);
+    }
+  }, [])
+
   return (
-    <div>HistoryOrderModal</div>
+    <ModalWrapper showModal={showModal} setShowModal={setShowModal} id='EditOrderModal'>
+      <FormBody />
+    </ModalWrapper>
+  )
+}
+
+const FormBody = () => {
+  return (
+    <div className="card-body">
+      <div className="basic-form">
+        <form>
+          <p>History</p>
+
+          <button type="submit" className="btn btn-primary">
+            Valider
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
--- src/Components/Table/Modal/Order/ReportOrderModal.tsx
+++ src/Components/Table/Modal/Order/ReportOrderModal.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ModalWrapper from '../ModalWrapper'
+import { CustumInput } from '../../../Forms';
 
-export default function ReportOrderModal() {
+interface Props {
+  showModal: boolean,
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+export default function ReportOrderModal({ showModal, setShowModal }: Props) {
+  
+  useEffect(() => {
+    var body = document.querySelector<HTMLBodyElement>('body');
+
+    var modalBackdrop = document.createElement('div');
+    modalBackdrop.className = 'modal-backdrop fade show';
+
+    if (body) {
+      body.classList.add('modal-open');
+      body.style.overflow = 'hidden';
+      body.style.paddingRight = '17px';
+
+      body.appendChild(modalBackdrop);
+    }
+  }, [])
+
   return (
-    <div>ReportOrderModal</div>
+    <ModalWrapper showModal={showModal} setShowModal={setShowModal} id='EditOrderModal'>
+      <FormBody />
+    </ModalWrapper>
+  )
+}
+
+const FormBody = () => {
+  return (
+    <div className="card-body">
+      <div className="basic-form">
+        <form>
+          <div className="row">
+            <CustumInput type={'date'} label={"Date de report"} placeholder={'Date de report'} />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Valider
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
