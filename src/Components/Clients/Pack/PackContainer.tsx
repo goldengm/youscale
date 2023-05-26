10f14a170a6acd98546373586aa5fc02cc5f1ba1
Oumardev <oumsdev@gmail.com>
2023-05-26 07:15:32 +0000

    fix show days left for subscription

--- src/Components/Clients/Pack/PackContainer.tsx
+++ src/Components/Clients/Pack/PackContainer.tsx
@@ -6,6 +6,7 @@ import { Account } from './Account'
 import { Transaction } from './Transaction'
 import { PaymentMethod } from './PaymentMethod'
 import { useGetClientPaymentMethodQuery } from '../../../services/api/ClientApi/ClientPaymentMethodApi'
+import { useGetClientPackQuery } from '../../../services/api/ClientApi/ClientPackApi'
 
 interface Bank {
     id: number;
@@ -13,7 +14,8 @@ interface Bank {
     bank: string;
     rib: string;
 }
-export default function Pack() {
+export default function PackContainer() {
+    const { data: dataPack, isLoading, refetch } = useGetClientPackQuery()
     const { data } = useGetClientPaymentMethodQuery()
     const [currentBank, setCurrentBank] = useState<Bank | undefined>()
 
@@ -21,12 +23,25 @@ export default function Pack() {
         setCurrentBank(data?.data[0] ? data?.data[0].Bank_Information : undefined)
     }, [data])
 
+    function computeDateDifference(startDate: Date, endDate: Date): number {
+        const millisecondsPerDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+
+        // Convert both dates to UTC to avoid issues with daylight saving time
+        const startUtc = Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
+        const endUtc = Date.UTC(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
+
+        // Calculate the difference in days
+        const diffInDays = Math.floor((endUtc - startUtc) / millisecondsPerDay);
+
+        return diffInDays;
+    }
+
     return (
         <Main name='Pack'>
             <div className="content-body">
                 <div className="container-fluid">
-                    <PackTitlte title='Pack' />
-                    <Tarif />
+                    {dataPack?.data && <PackTitlte title={`Pack <p class='expire-txt'>your pack will expire in ${computeDateDifference(new Date(), new Date(dataPack.data.Subscription.date_expiration) )} days</p>`} />}
+                    <Tarif data={dataPack} isLoading={isLoading} refetch={refetch} />
                     <PackTitlte title='Payment methods' />
                     <Account />
                     <PaymentMethod data={data?.data} setCurrentBank={setCurrentBank} />
@@ -45,7 +60,7 @@ const PackTitlte = ({ title }: PackTitlteProps): JSX.Element => {
         <div className="row page-titles">
             <ol className="breadcrumb">
                 <li className="breadcrumb-item active">
-                    <a href="javascript:void(0)">{title}</a>
+                    <a dangerouslySetInnerHTML={{ __html: title }} href="javascript:void(0)"></a>
                 </li>
             </ol>
         </div>
