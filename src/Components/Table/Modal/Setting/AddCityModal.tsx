ae57c204b9ab77fca66966a15d15d95c5a702600
Oumardev <oumsdev@gmail.com>
2023-07-11 20:33:21 +0000

    fix shipping companies url

--- src/Components/Table/Modal/Setting/AddCityModal.tsx
+++ src/Components/Table/Modal/Setting/AddCityModal.tsx
@@ -1,22 +1,45 @@
 import React, { useEffect, useState } from 'react'
-import { CustumInput } from '../../../Forms'
+import { CustumInput, CustumSelectForm } from '../../../Forms'
 import ModalWrapper from '../ModalWrapper'
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 import { useAddCityMutation } from '../../../../services/api/ClientApi/ClientCityApi';
 import { showToastError } from '../../../../services/toast/showToastError';
-import { ErrorModel } from '../../../../models';
+import { ErrorModel, ShippingModel } from '../../../../models';
+import { useGetShippingQuery } from '../../../../services/api/ClientApi/ClientShippingApi';
 
 
 type Inputs = {
     name: string,
     price: string
+    id_shipping: number
 };
+
+type SelectType = {
+    label: string,
+    value: string | number
+}
+
+const GetShippingCompanies = (data: ShippingModel[] | undefined): SelectType[] => {
+    if (!data) return []
+
+    var newArr: SelectType[] = [{ label: 'none', value: 'none' }]
+
+    for (let i = 0; i < data.length; i++) {
+        newArr.push({
+            value: data[i].id ?? 0,
+            label: data[i].name
+        })
+    }
+
+    return newArr
+}
 
 const schema = yup.object().shape({
     name: yup.string().required('Ce champ est obligatoire'),
-    price: yup.string().required('Ce champ est obligatoire')
+    price: yup.string().required('Ce champ est obligatoire'),
+    id_shipping: yup.number().required('Ce champ est obligatoire')
 }).required();
 
 interface Props {
@@ -69,6 +92,7 @@ interface FormBodyProps {
     handleCloseModal: () => void
 }
 const FormBody = ({ refetch, handleCloseModal }: FormBodyProps) => {
+    const { data: shippingData } = useGetShippingQuery()
     const [addCity] = useAddCityMutation()
 
     const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
@@ -82,7 +106,7 @@ const FormBody = ({ refetch, handleCloseModal }: FormBodyProps) => {
                 refetch()
                 handleCloseModal()
             })
-            .catch((err: {data: ErrorModel | {message : string}, status: number}) => {
+            .catch((err: { data: ErrorModel | { message: string }, status: number }) => {
                 if (err.data) {
                     if ('errors' in err.data && Array.isArray(err.data.errors) && err.data.errors.length > 0) showToastError(err.data.errors[0].msg);
                     else if ('message' in err.data) showToastError(err.data.message);
@@ -111,6 +135,14 @@ const FormBody = ({ refetch, handleCloseModal }: FormBodyProps) => {
                             type={'text'}
                             label={"Price"}
                             placeholder={'Prix'}
+                        />
+
+                        <CustumSelectForm
+                            data={GetShippingCompanies(shippingData?.data)}
+                            register={register}
+                            error={errors.id_shipping}
+                            label={"Shipping companies"}
+                            name={'id_shipping'}
                         />
                     </div>
 
