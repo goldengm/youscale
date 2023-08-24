839639e3560aeca4709c8669e0c1eb5fa181a8b0
Oumardev <oumsdev@gmail.com>
2023-08-24 09:38:34 +0000

    feat support

--- src/Components/Clients/Support/Support.tsx
+++ src/Components/Clients/Support/Support.tsx
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from 'react'
 import { SupportTable } from '../../Table/Support'
-import io from 'socket.io-client'
 import Main from '../../Main'
 
-export default function Product(): JSX.Element {
+export default function Support(): JSX.Element {
 
     const closeTutorial = () => { };
-
-    useEffect(() => {
-        const socket = io('http://127.0.0.1:8500', { transports: ['websocket'] })
-    }, [])
 
     return (
         <Main
