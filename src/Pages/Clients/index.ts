7dd9a02686f1da536511803baf1ad548a3cce628
Oumardev <oumsdev@gmail.com>
2023-09-14 15:42:00 +0000

    fix bug & fix auth ux/ui

--- src/Pages/Clients/index.ts
+++ src/Pages/Clients/index.ts
@@ -8,12 +8,13 @@ import PackPage from "./PackPage";
 import RegisterPage from "./RegisterPage";
 import OPTVerificationPage from "./OPTVerificationPage";
 import ForgotPasswordPage from "./ForgotPasswordPage";
-import ChoosePackPage from "./ChoosePackPage";
+//import ChoosePackPage from "./ChoosePackPage";
 import SupportPage from "./SupportPage";
 import SuccessPage from "./Success/SuccessPage";
 import ErrorPage from "./ErrorPage";
 import LoginPage from "./Login/LoginPage";
 import QuestionPage from "./Question/QuestionPage";
+import ChoosePackPage from "./Pack/ChoosePackPage";
 
 export { 
     DashbordPage,
