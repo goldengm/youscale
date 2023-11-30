4520b10e01a3dbadfeada9c5f19763b26d2858ea
simba830 <realdd521@gmail.com>
2023-11-30 13:11:28 -0500

    [fixed] react-fast-marquee

--- src/Components/Main/Banner.tsx
+++ src/Components/Main/Banner.tsx
@@ -3,7 +3,7 @@ import { FaArrowLeft, FaArrowRight, FaTimes } from "react-icons/fa";
 import { useGetAnnoucementQuery } from "../../services/api/ClientApi/ClientAnnoucementApi";
 import { AdminAnnoucementModel } from "../../models";
 import styles from "./HeaderBanner.module.css";
-import Marquee from "react-marquee-slider";
+import Marquee from "react-fast-marquee";
 
 const getAnnoucement = (
   data: AdminAnnoucementModel[] | undefined
