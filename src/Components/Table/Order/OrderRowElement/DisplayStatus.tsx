d441b4005c598f347dd9513804dd7dcd12f09b7a
Talha Hameed <talha4050@gmail.com>
2024-01-01 19:10:54 +0500

    fix filters and change status for order page

--- src/Components/Table/Order/OrderRowElement/DisplayStatus.tsx
+++ src/Components/Table/Order/OrderRowElement/DisplayStatus.tsx
@@ -7,21 +7,22 @@ interface Props {
     onChange: (e: React.ChangeEvent<HTMLSelectElement>) => any,
     currentData: GetClientOrderModel,
 }
-export default function DisplayStatus( { name, onChange, statusData, currentData }: Props): JSX.Element {
+export default function DisplayStatus({ name, onChange, statusData, currentData }: Props): JSX.Element {
 
-    const expedie_item = statusData && statusData.find(stu=> stu.name === 'Expedie livraison')
+    const expedie_item = statusData && statusData.find(stu => stu.name === 'Expedie livraison')
 
     return (
         <select
             onChange={onChange}
             className="select-custum"
+            value={currentData?.Status || ''}
         >
-            { expedie_item && <option selected={expedie_item.name === currentData.Status} style={{ color:expedie_item.color ?? 'black'}} value={expedie_item.name}>{expedie_item.name}</option> }
+            {expedie_item && <option selected={expedie_item.name === currentData.Status} style={{ color: expedie_item.color ?? 'black' }} value={expedie_item.name}>{expedie_item.name}</option>}
             {
                 statusData && statusData.map(
                     (dt) => {
-                        if(dt.name === 'Expedie livraison') return 
-                        return <option selected={dt.name === currentData.Status} style={{ color:dt.color ?? 'black'}} value={dt.name}>{dt.name}</option>
+                        if (dt.name === 'Expedie livraison') return
+                        return <option selected={dt.name === currentData.Status} style={{ color: dt.color ?? 'black' }} value={dt.name}>{dt.name}</option>
                     })
             }
         </select>
